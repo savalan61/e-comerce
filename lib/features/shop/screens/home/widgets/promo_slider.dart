f6df3d464909b4e7f36c495aeb2845ea13e23c08
savalan61 <savalandaglari2020@gmail.com>
2024-09-24 19:59:24 +0300

    login problem finished

--- lib/features/shop/screens/home/widgets/promo_slider.dart
+++ lib/features/shop/screens/home/widgets/promo_slider.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
